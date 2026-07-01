a037d882966d9dbee24d99e686b19072701b7782
AMAL ANAN <amalananana@gmail.com>
2026-07-01 12:48:22 +0300

    main function

---
+++ bin/flutter_training_dart.dart
@@ -0,0 +1,7 @@
+// import 'package:flutter_training_dart/flutter_training_dart.dart' as flutter_training_dart;
+//
+// void main(List<String> arguments) {
+//   print('Hello world: ${flutter_training_dart.calculate()}!');
+// }
+
+void main() {}

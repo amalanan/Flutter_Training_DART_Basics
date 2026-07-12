4a64385d6e61beb721e360f072cfbda0179a4563
AMAL ANAN <amalananana@gmail.com>
2026-07-12 12:22:44 +0300

    super constructor

--- bin/animal.dart
+++ bin/animal.dart
@@ -1,6 +1,10 @@
 //inheritance
 class Animal {
   int numberOfLimbs = 4;
+  String? skinColor;
+  double? weight;
+
+  Animal(this.skinColor, this.weight);
 
   eat() {
     print('eating');
@@ -8,18 +12,24 @@ class Animal {
 }
 
 class Dog extends Animal {
+  Dog(super.skinColor, super.weight);
+
   bark() {
     print('barking');
   }
 }
 
 class Cat extends Animal {
+  Cat(super.skinColor, super.weight);
+
   meo() {
     print('meowing');
   }
 }
 
 class Lion extends Animal {
+  Lion(super.skinColor, super.weight);
+
   roar() {
     print('roaring');
   }
